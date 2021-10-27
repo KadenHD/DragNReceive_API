61e173731f57056e3e78f9b6948c14e0c79c7ca8
KadenHD <71968563+KadenHD@users.noreply.github.com>
2021-10-27 18:37:43 +0200

    rename

---
+++ Controllers/Users.js
@@ -0,0 +1,47 @@
+import { v4 as uuidv4 } from 'uuid';
+
+/* Temporary Users */
+// Delete this once the DataBase is build
+let users = [];
+/* \Temporary Users */
+
+export const getUsers = (req, res) => {
+    res.send(users);
+}
+
+export const createUser = (req, res) => {
+    const user = req.body;
+
+    users.push({ ...user, id: uuidv4() }); // génère un id unique);
+
+    res.send(`${user.firstName} ${user.lastName} added to the DataBase`);
+}
+
+export const getUser = (req, res) => {
+    const { id } = req.params;
+
+    const user = users.find((user) => user.id === id);
+
+    res.send(user);
+}
+
+export const deleteUser = (req, res) => {
+    const { id } = req.params;
+
+    users = users.filter((user) => user.id !== id);
+
+    res.send(`User with the id ${id} deleted from the DataBase`);
+}
+
+export const updateUser = (req, res) => {
+    const { id } = req.params;
+    const { firstName, lastName, age } = req.body;
+
+    const user = users.find((user) => user.id === id);
+
+    if (firstName) user.firstName = firstName;
+    if (lastName) user.lastName = lastName;
+    if (age) user.age = age;
+
+    res.send(`User with the id ${id} patched elements !`);
+}
