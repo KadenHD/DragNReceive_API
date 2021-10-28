c1286a7dfba8742bd10f62a083efdbbf53669af4
KadenHD <71968563+KadenHD@users.noreply.github.com>
2021-10-28 18:06:32 +0200

    build auth controller

--- Routes/Routes.js
+++ Routes/Routes.js
@@ -1,9 +1,11 @@
 import express from 'express';
 
+import { loginUser } from '../Controllers/Auth.js';
+
 const router = express.Router();
 
 router.get('/', (req, res) => res.send('Hello from Homepage.'));
 
-router.post('/login', (req, res) => res.send('Hello from Login.'));
+router.post('/login', loginUser);
 
 export default router;
