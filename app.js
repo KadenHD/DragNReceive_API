0b70790b3378d89545870274970d8b3621ba1cd5
KadenHD <71968563+KadenHD@users.noreply.github.com>
2021-10-28 14:01:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ const app = express();
 app.use(express.json()) //For JSON requests
 app.use(express.urlencoded({ extended: true }));
 
+/* Define Routes */
 app.use('/users', usersRoutes);
-
 app.get('/', (req, res) => res.send('Hello from Homepage.'));
 
 var port = process.env.PORT || 3000;
