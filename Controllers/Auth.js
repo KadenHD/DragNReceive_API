4fbdfc3f4bf20d25804a0799c86c77ab9a1584fe
KadenHD <71968563+KadenHD@users.noreply.github.com>
2021-10-31 14:20:52 +0100

    Update Auth.js

--- Controllers/Auth.js
+++ Controllers/Auth.js
@@ -2,27 +2,10 @@ import bcrypt from 'bcrypt';
 
 import User from '../Models/User.js'
 
-export const loginUser = async (req, res) => {
-    const user = await User.findOne({ where: { email: req.body.email } })
-    if (!user) return res.status(400).json({
-        message: 'Wrong email'
-    });
-    try {
-        if (await bcrypt.compare(req.body.password, user.password)) {
-            // Do the token creation and auth
-            res.json({
-                message: 'You are connected'
-            });
-        } else {
-            res.status(400).json({
-                message: 'Wrong password'
-            });
-        }
-    } catch {
-        res.status(500).json();
-    }
+export const loginUser = (req, res) => {
+
 }
 
 export const currentUser = (req, res) => {
-
+    
 }
