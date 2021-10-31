48b4b24c4bd5e27aee11f6d4a87ae7f0ab494dca
KadenHD <71968563+KadenHD@users.noreply.github.com>
2021-10-31 20:56:22 +0100

    finally create jwt but need to do the refresh token etc

--- Controllers/Auth.js
+++ Controllers/Auth.js
@@ -1,11 +1,45 @@
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { extractBearerToken } from '../Middlewares/Token.js';
 
 import User from '../Models/User.js'
 
-export const loginUser = (req, res) => {
+export const loginUser = async (req, res) => {
+    // Get user and Check email
+    const user = await User.findOne({
+        where: {
+            email: req.body.email
+        }
+    });
+    if(!user) return res.status(400).json({message: `Email not found`});
 
+    // Check Password
+    const checkPassword = await bcrypt.compareSync(req.body.password, user.password);
+    if(!checkPassword) return res.status(400).json({message: `Invalid Password`});
+
+    // Create User token
+    const token = jwt.sign(
+        {
+            id: user.id,
+        }, 
+        process.env.SECRET_TOKEN
+    );
+    res.header('token', token).json({token: token})
 }
 
-export const currentUser = (req, res) => {
+export const currentUser = async (req, res) => {
+    const token = req.headers.authorization && extractBearerToken(req.headers.authorization)
+    const decoded = jwt.decode(token, {complete: false})
 
+    const getUser = await User.findByPk(decoded.id)
+    
+    const user = {
+        id: getUser.id,
+        lastname: getUser.lastname,
+        firstname: getUser.firstname,
+        email: getUser.email,
+        roleId: getUser.roleId
+    }
+
+    return res.status(200).json({user})
 }
--- Middlewares/Token.js
+++ Middlewares/Token.js
@@ -1,5 +1,22 @@
 import jwt from 'jsonwebtoken';
 
 export const CheckToken = (req, res, next) => {
-    
+    const token = req.headers.authorization && extractBearerToken(req.headers.authorization);
+    if (!token) return res.status(401).json({message: `You need a token`});
+
+    try {
+        req.user = jwt.verify(token, process.env.SECRET_TOKEN);
+        next();
+    } catch (error) {
+        res.status(400).json({message: `Token not valid`});
+    }
+}
+
+export const extractBearerToken = (headerValue) => {
+    if (typeof headerValue !== 'string') {
+        return false
+    }
+
+    const matches = headerValue.match(/(bearer)\s+(\S+)/i)
+    return matches && matches[2]
 }
