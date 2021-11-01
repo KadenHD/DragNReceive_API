e9be4f0746bac052366c0f46f240a982fbd145c4
KadenHD <71968563+KadenHD@users.noreply.github.com>
2021-11-01 21:44:51 +0100

    Update Auth.js

--- Controllers/Auth.js
+++ Controllers/Auth.js
@@ -11,11 +11,11 @@ export const loginUser = async (req, res) => {
             email: req.body.email
         }
     });
-    if(!user) return res.status(400).json({message: `Email not found`});
+    if(!user) return res.status(400).json({error: `Email not found`});
 
     // Check Password
     const checkPassword = await bcrypt.compareSync(req.body.password, user.password);
-    if(!checkPassword) return res.status(400).json({message: `Invalid Password`});
+    if(!checkPassword) return res.status(400).json({error: `Invalid Password`});
 
     // Create User token
     const token = jwt.sign(
