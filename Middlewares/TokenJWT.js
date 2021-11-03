4ea7a8e79ea3b0731d970a7c8d20c1ece6599cf5
KadenHD <71968563+KadenHD@users.noreply.github.com>
2021-11-03 14:06:11 +0100

    Update TokenJWT.js

--- Middlewares/TokenJWT.js
+++ Middlewares/TokenJWT.js
@@ -2,13 +2,13 @@ import jwt from 'jsonwebtoken';
 
 export const CheckToken = (req, res, next) => {
     const token = req.headers.authorization && extractBearerToken(req.headers.authorization);
-    if (!token) return res.status(401).json({message: `You need a token`});
+    if (!token) return res.status(401).json({error: `You need a token`});
 
     try {
         req.user = jwt.verify(token, process.env.SECRET_TOKEN);
         next();
     } catch (error) {
-        res.status(400).json({message: `Token not valid`});
+        res.status(400).json({error: `Token not valid`});
     }
 }
 
