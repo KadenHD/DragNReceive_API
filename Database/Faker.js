dfb019660ec706635808d622f0e9d191f0b3b62d
KadenHD <71968563+KadenHD@users.noreply.github.com>
2021-12-16 21:07:44 +0100

    init Fakes value

--- Database/Faker.js
+++ Database/Faker.js
@@ -2,106 +2,109 @@ import faker from 'faker';
 import bcrypt from 'bcrypt';
 import { v4 as uuidv4 } from 'uuid';
 
+import { Logo, Shop, Product, Role, User, Order, Ticket, Message } from '../Models/Models.js';
+
 faker.locale = "fr";
 
-let fakerLogos = [];
-let fakerShops = [];
-let fakerProducts = [];
-let fakerRoles = [];
-let fakerDefaultUsers = [];
-let fakerUsers = [];
-let fakerOrders = [];
-let fakerTickets = [];
-let fakerMessages = [];
+try {
+    defaultUsers();
+} catch (error) {
+    console.log(error);
+} finally {
+    console.log("DataBase instance and fake values created !");
+}
 
-for (let i = 1; i < 11; i++) {
-    let shopId = uuidv4();
-    let roleId = faker.datatype.number({min: 1, max: 4});
-    let userId = uuidv4();
-    let productId = uuidv4();
+//Create the fakes standards users
+async function defaultUsers() {
+    for(let i = 1; i<5 ; i++) {
+        switch (i) {
+            case 1:
+                const hashedSadmin = await bcrypt.hash("sadmin", 10);
+                Role.create({ id: i, label: "SUPERADMIN" });
+                User.create({ id: i, lastname: "sadmin", firstname: "sadmin", email: "sadmin@sadmin.sadmin", password: hashedSadmin, roleId: i, shopId: null });
+                break;
+            case 2:
+                const hashedAdmin = await bcrypt.hash("admin", 10);
+                Role.create({ id: i, label: "ADMIN" });
+                User.create({ id: i, lastname: "admin", firstname: "admin", email: "admin@admin.admin", password: hashedAdmin, roleId: i, shopId: null });
+                break;
+            case 3:
+                const hashedPartner = await bcrypt.hash("partner", 10);
+                Role.create({ id: i, label: "PARTNER" });
+                User.create({ id: i, lastname: "partner", firstname: "partner", email: "partner@partner.partner", password: hashedPartner, roleId: i, shopId: null });
+                break;
+            case 4:
+                const hashedUser = await bcrypt.hash("user", 10);
+                Role.create({ id: i, label: "USER" });
+                User.create({ id: i, lastname: "user", firstname: "user", email: "user@user.user", password: hashedUser, roleId: i, shopId: null });
+                break;
+        }
+    }
+}
 
-    let logo = { 
+async function fakeLogo() {
+    const logoId = uuidv4();
+    Logo.create({
+        id: logoId,
+        name: "",
+        size: "",
+        type: "",
+        bin: ""
+    });
+    return logoId;
+}
 
-    };
-    fakerLogos[i] = logo;
-
-    let shop = {
+async function fakeShop(logoId) {
+    const shopId = uuidv4();
+    Shop.create({
         id: shopId, 
         name: faker.company.companyName(), 
         phone: faker.phone.phoneNumberFormat(), 
         city: faker.address.cityName(), 
         street: faker.address.streetAddress(), 
         postal: faker.address.zipCode(),
-        logoId: null
-    };
-    fakerShops[i] = shop;
-
-    let product = {
-        id: productId,
-        name: "",
-        description: "",
-        price: 2,
-        stock: 5,
-        shopId: shopId,
-    };
-    fakerProducts[i] = product;
-
-    if (roleId !== 3) {
-        shopId = null;
-    }
-
-    let user = {
-        id: userId,
-        lastname: faker.name.lastName(),
-        firstname: faker.name.firstName(),
-        email: faker.internet.email(),
-        password: await bcrypt.hash(faker.internet.password(), 10),
-        roleId: roleId,
-        shopId: shopId
-    };
-    fakerUsers[i] = user;
-
-    let order = {
-
-    };
-    fakerOrders[i] = order;
-
-    let ticket = {
-
-    };
-    fakerTickets[i] = ticket;
-
-    let message = {
-
-    };
-    fakerMessages[i] = message;
+        logoId: logoId
+    })
+    return shopId;
 }
-fakerRoles = [
-    { id: "1", label: "SUPERADMIN" }, 
-    { id: "2", label: "ADMIN" }, 
-    { id: "3", label: "PARTNER" }, 
-    { id: "4", label: "USER" }
-];
-// Hash the password
-const hashedSadmin = await bcrypt.hash("sadmin", 10);
-const hashedAdmin = await bcrypt.hash("admin", 10);
-const hashedPartner = await bcrypt.hash("partner", 10);
-const hashedUser = await bcrypt.hash("user", 10);
-fakerDefaultUsers = [
-    { id: '1', lastname: "sadmin", firstname: "sadmin", email: "sadmin@sadmin.sadmin", password: hashedSadmin, roleId: "1", shopId: null },
-    { id: '2', lastname: "admin", firstname: "admin", email: "admin@admin.admin", password: hashedAdmin, roleId: "2", shopId: null },
-    { id: '3', lastname: "partner", firstname: "partner", email: "partner@partner.partner", password: hashedPartner, roleId: "3", shopId: null },
-    { id: '4', lastname: "user", firstname: "user", email: "user@user.user", password: hashedUser, roleId: "4", shopId: null }
-];
 
-export const logos = fakerLogos;
-export const shops = fakerShops;
-export const products = fakerProducts;
-export const roles = fakerRoles;
-export const defaultUsers = fakerDefaultUsers;
-export const users = fakerUsers;
-export const orders = fakerOrders;
-export const tickets = fakerTickets;
-export const messages = fakerMessages;
+async function fakeProduct(shopId) {
+    const productId = uuidv4();
+    Product.create({
+        id: productId,
+        name: faker.commerce.productName(),
+        description: faker.commerce.productDescription(),
+        price: faker.datatype.float(),
+        stock: faker.datatype.number({min: 1, max: 100}),
+        shopId: shopId,
+    });
+    return productId;
+}
 
-export default { logos, shops, products, roles, defaultUsers, users, orders, tickets, messages };
+async function fakeOrder(userId, productId) {
+    const orderId = uuidv4();
+    Order.create({
+        id: orderId,
+        quantities: faker.datatype.number({min: 1, max: 100}),
+        number: uuidv4(),
+        userId: userId,
+        productId: productId
+    });
+    return orderId;
+}
+
+async function fakeTicket() {
+    const ticketId = uuidv4();
+    Ticket.create({
+
+    });
+    return ticketId;
+}
+
+async function fakeMessage() {
+    const messageId = uuidv4();
+    Message.create({
+
+    });
+    return messageId;
+}
