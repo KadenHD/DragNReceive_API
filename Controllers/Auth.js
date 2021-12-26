39d7eeb2ea19ae098f55918e5c838022f140b489
KadenHD <pierraored@gmail.com>
2021-12-26 20:01:06 +0100

    Auth/register controller and permissions

--- Controllers/Auth.js
+++ Controllers/Auth.js
@@ -7,6 +7,21 @@ export const loginUser = async (req, res) => {
     const user = await User.findOne({ where: { email: req.body.email } });
     if (!user) return res.status(400).json({ error: `Email introuvable.` });
 
+    if (user.roleId === "4") return res.status(400).json({ error: `Vous ne pouvez pas vous connecter avec un compte client, utilisez notre application mobile !` });
+
+    const checkPassword = bcrypt.compareSync(req.body.password, user.password);
+    if (!checkPassword) return res.status(400).json({ error: `Mot de passe incorrect.` });
+
+    const token = jwt.sign({ id: user.id }, process.env.SECRET_TOKEN);
+    res.header('token', token).json({ token: token, user });
+}
+
+export const loginClient = async (req, res) => {
+    const user = await User.findOne({ where: { email: req.body.email } });
+    if (!user) return res.status(400).json({ error: `Email introuvable.` });
+
+    if (user.roleId != "4") return res.status(400).json({ error: `Vous ne pouvez que vous connecter avec un compte client, utilisez notre site web !` });
+
     const checkPassword = bcrypt.compareSync(req.body.password, user.password);
     if (!checkPassword) return res.status(400).json({ error: `Mot de passe incorrect.` });
 
--- Routes/Auths.js
+++ Routes/Auths.js
@@ -1,9 +1,14 @@
 import express from 'express';
 
-import { loginUser } from '../Controllers/Auth.js';
+import { loginUser, loginClient } from '../Controllers/Auth.js';
+import { createUser } from '../Controllers/User.js';
+import { validFormCreateUser } from '../Permissions/Users.js';
+import { authRegisterClient } from '../Permissions/Auth.js';
 
 const router = express.Router();
 
-router.post('/login', loginUser);
+router.post('/loginUser', loginUser);
+router.post('/loginClient', loginClient);
+router.post('/register', authRegisterClient, validFormCreateUser, createUser);
 
 export default router;
