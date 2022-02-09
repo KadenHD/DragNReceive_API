8879e604d9ab1828f66341fa84230f48adc348dc
KadenHD <71968563+KadenHD@users.noreply.github.com>
2022-02-09 17:30:31 +0100

    Update Auth.js

--- Controllers/Auth.js
+++ Controllers/Auth.js
@@ -18,7 +18,7 @@ export const loginUser = async (req, res) => {
 export const loginClient = async (req, res) => {
     const user = await User.findOne({ where: { email: req.body.email } });
     if (!user) return res.status(400).json({ error: `Email introuvable.` });
-    if (user.roleId != "4") return res.status(400).json({ error: `Vous ne pouvez que vous connecter avec un compte client, utilisez notre site web !` });
+    if (user.roleId != "4") return res.status(400).json({ error: `Vous ne pouvez pas vous connecter avec un compte partenaire ou administrateur, utilisez notre site web !` });
     const checkPassword = bcrypt.compareSync(req.body.password, user.password);
     if (!checkPassword) return res.status(400).json({ error: `Mot de passe incorrect.` });
     const token = jwt.sign({ id: user.id }, process.env.SECRET_TOKEN);
