cfb5efb618639a446fb2b27d86295a315e109935
KadenHD <pierraored@gmail.com>
2022-02-21 21:40:48 +0100

    Update BuildDatabase.js

--- Database/BuildDatabase.js
+++ Database/BuildDatabase.js
@@ -2,12 +2,15 @@ import sequelize from './DBConfig.js';
 
 import { Shop, Product, Role, User, Order, Ticket, Message, TicketStatus, OrderStatus } from '../Models/Models.js'; /* KeepIt */
 
+console.log("INFO: Creating Database...")
 sequelize
     .sync({ force: true })
     .then(created => {
-        console.log("INFO: Database Successfully Created.")
+        console.log("SUCCESS: Database Successfully Created.")
         process.exit();
     })
     .catch(err => {
+        console.log("ERROR: " + err)
+        console.log("ERROR: Try to launch your Laragon, Xamp or Wamp server.")
         process.exit();
     });
