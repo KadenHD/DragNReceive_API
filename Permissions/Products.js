0a7da83a8fac5f87df9414a6c1e9fb9585e15605
KadenHD <pierraored@gmail.com>
2022-02-24 18:10:02 +0100

    Update Products.js

--- Permissions/Products.js
+++ Permissions/Products.js
@@ -73,7 +73,7 @@ export const validFormCreateProduct = async (req, res, next) => {
 
 export const validFormUpdateProduct = async (req, res, next) => {
     if (!req.body.name || !req.body.description || !req.body.price || !req.body.stock) return res.status(403).json({ error: `Le formulaire n'est pas bon !` });
-    if ((req.body.name == req.product.name) && (req.body.description == req.product.description) && (req.body.price == req.product.price) && (req.body.stock == req.product.stock) && (!req.files)) return res.status(401).json({ error: `Vous n'avez pas modifiée le produit !` });
+    if ((req.body.name == req.product.name) && (req.body.description == req.product.description) && (req.body.price == req.product.price) && (req.body.stock == req.product.stock) && (!req.files)) return res.status(401).json({ error: `Vous n'avez pas modifié le produit !` });
     if (req.body.name != req.product.name) {
         if (!isValidName(req.body.name)) return res.status(403).json({ error: `Format de nom non-valide !` });
         req.product.name = req.body.name;
