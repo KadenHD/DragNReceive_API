25ac146f8ccef284548e0844ba92e77609ba1a1b
KadenHD <pierraored@gmail.com>
2022-03-02 19:36:05 +0100

    Update Messages.js

--- Validations/Messages.js
+++ Validations/Messages.js
@@ -5,8 +5,9 @@ const client = "4";
 
 export const canCreateMessage = (currentUser, ticket) => {
     return (
-        currentUser.roleId === sadmin || /* Is sadmin */
-        currentUser.roleId === admin || /* Is admin */
-        currentUser.id === ticket.userId
+        (currentUser.roleId === sadmin || /* Is sadmin */
+            currentUser.roleId === admin || /* Is admin */
+            currentUser.id === ticket.userId) &&/* is own */
+        ticket.ticketStatusId === "1"/* ticket is not closed */
     );
 }
