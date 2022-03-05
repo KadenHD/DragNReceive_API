36a88ab24431ae076f46f1eeb6608c8476a6cb06
KadenHD <pierraored@gmail.com>
2022-03-05 15:43:11 +0100

    Update Message.js

--- Controllers/Message.js
+++ Controllers/Message.js
@@ -1,15 +1,24 @@
-import { Message } from '../Models/Models.js';
+import { Message, Ticket } from '../Models/Models.js';
 
 import { responsedTicket } from '../Scripts/NodeMailer.js';
 
 export const createMessage = (req, res) => {
     Message.create(req.body)
         .then(data => {
-            console.log(req.ticket)
-            if (req.currentUser.id != req.ticket.userId) { responsedTicket(req.ticket, req.ticket.dataValues.user) }
-            res.status(200).json({
-                success: `Le message a bien été créé.`
-            });
+            req.ticket.updatedAt = new Date()
+            Ticket.update(req.ticket.dataValues, { where: { id: req.ticket.id } })
+                .then(data => {
+                    if (req.currentUser.id != req.ticket.userId) { responsedTicket(req.ticket, req.ticket.dataValues.user) }
+                    res.status(200).json({
+                        success: `Le message a bien été créé.`
+                    });
+                })
+                .catch(err => {
+                    res.status(500).json({
+                        error: `Une erreur est survenue lors de la création du message.`
+                    });
+                })
+
         })
         .catch(err => {
             res.status(500).json({
