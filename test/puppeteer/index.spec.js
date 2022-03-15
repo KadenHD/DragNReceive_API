0cd66dc663d3bd8124a02f3022c552a421c28f20
KadenHD <pierraored@gmail.com>
2022-03-15 01:29:44 +0100

    Create index.spec.js

---
+++ test/puppeteer/index.spec.js
@@ -0,0 +1,13 @@
+import puppeteer from 'puppeteer';
+
+import loginSpec from './login.spec.js';
+import createTicketSpec from './create-ticket.spec.js';
+
+const browser = await puppeteer.launch({ headless: false });
+
+/* 
+await loginSpec(browser);
+await createTicketSpec(browser);
+*/
+
+await browser.close();
