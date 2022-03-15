39f10569e9d70cab03e624543fddb0d3bfdfb8d8
KadenHD <pierraored@gmail.com>
2022-03-15 21:41:25 +0100

    Update index.spec.js

--- test/puppeteer/index.spec.js
+++ test/puppeteer/index.spec.js
@@ -1,11 +1,15 @@
 import puppeteer from 'puppeteer';
+import dotenv from 'dotenv';
+dotenv.config();
 
 import sadminSpec from './sadmin.spec.js';
 import partnerSpec from './partner.spec.js';
 
 const browser = await puppeteer.launch({ headless: false });
+const url = process.env.PUPPETEER_WEB_URL;
+const delay = 1000;
 
-await sadminSpec(browser);
-await partnerSpec(browser);
+await partnerSpec(browser, url, delay);
+// await sadminSpec(browser, url, delay);
 
 await browser.close();
