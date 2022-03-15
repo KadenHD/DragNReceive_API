cac2db1da0a93ecdfebcf271e4426dbb8124833f
KadenHD <pierraored@gmail.com>
2022-03-15 21:41:22 +0100

    Create index.js

---
+++ test/puppeteer/index.js
@@ -0,0 +1,44 @@
+export const initialize = async (browser, url) => {
+    /* Initialize */
+    const page = await browser.newPage();
+    await page.setViewport({ width: 900, height: 900 });
+    await page.goto(url, { waitUntil: "networkidle2" });
+    return page;
+}
+
+export const login = async (page, delay, email, password) => {
+    /* Go to login */
+    await page.click("#drawer", { delay: delay });
+    await page.waitForSelector("#notlogged", { visible: true });
+    await page.click("#notlogged", { delay: delay });
+    await page.waitForSelector("#Login", { visible: true });
+    await page.click("#Login", { delay: delay });
+
+    /* Login */
+    await page.type("input[type=text]", email, { delay: 50 });
+    await page.type("input[type=password]", password, { delay: 50 });
+    await page.click("#submit", { delay: delay });
+    await page.waitForNavigation();
+}
+
+export const logout = async (page, delay) => {
+    /* Logout */
+    await page.click("#drawer", { delay: delay });
+    await page.waitForSelector("#logged", { visible: true });
+    await page.click("#logged", { delay: delay });
+    await page.waitForSelector("#logout", { visible: true });
+    await page.click("#logout", { delay: delay });
+
+    /* Close the script */
+    await page.waitForNavigation();
+    await page.waitForTimeout(5000);
+    await page.close();
+}
+
+export const clearInput = async (page, element) => {
+    await page.focus(element);
+    await page.keyboard.down('Control');
+    await page.keyboard.press('A');
+    await page.keyboard.up('Control');
+    await page.keyboard.press('Backspace');
+}
