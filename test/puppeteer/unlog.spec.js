aa0a3e8744e2f8c2a92acf7bbbd9a4fcf255b42b
KadenHD <pierraored@gmail.com>
2022-03-17 18:00:47 +0100

    Create unlog.spec.js

---
+++ test/puppeteer/unlog.spec.js
@@ -0,0 +1,9 @@
+import { initialize } from "./index.js";
+import { } from './unlog.js';
+
+const unlogSpec = async (browser, url, delay) => {
+    const page = await initialize(browser, url);
+    //
+};
+
+export default unlogSpec;
