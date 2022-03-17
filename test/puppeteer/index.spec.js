c741b17456b80069f765791879d5af45e3ba6c79
KadenHD <pierraored@gmail.com>
2022-03-17 13:07:47 +0100

    Update index.spec.js

--- test/puppeteer/index.spec.js
+++ test/puppeteer/index.spec.js
@@ -1,4 +1,5 @@
 import puppeteer from 'puppeteer';
+import ChalkMSG from '../../Scripts/Chalk.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -9,7 +10,16 @@ const browser = await puppeteer.launch({ headless: false });
 const url = process.env.PUPPETEER_WEB_URL;
 const delay = 1000;
 
-await partnerSpec(browser, url, delay);
-// await sadminSpec(browser, url, delay);
+try {
+    console.log(`${ChalkMSG.INFO}Launching the Puppeteer test...`)
+    await partnerSpec(browser, url, delay);
+    // await sadminSpec(browser, url, delay);
+    await browser.close();
+    console.log(`${ChalkMSG.SUCCESS}The Puppeteer test is successfully done.`)
+} catch {
+    console.log(`${ChalkMSG.ERROR}An error occured during the Puppeteer test.`)
+    await browser.close();
+}
 
-await browser.close();
+
+
