40086794f51b74fb16b6a77bcfbbd4924d9635bc
KadenHD <pierraored@gmail.com>
2022-03-19 17:37:20 +0100

    Update sadmin.spec.js

--- test/puppeteer/sadmin.spec.js
+++ test/puppeteer/sadmin.spec.js
@@ -1,11 +1,14 @@
 import { initialize, login, logout } from "./index.js";
-import { } from './sadmin.js';
+import { profile, user, shop, ticket } from './sadmin.js';
 
 const sadminSpec = async (browser, url, delay) => {
     const page = await initialize(browser, url);
     await login(page, delay, "sadmin@sadmin.sadmin", "Password1@");
 
-    //
+    await profile(page, delay);
+    await user(page, delay);
+    await shop(page, delay);
+    await ticket(page, delay);
 
     await logout(page, delay);
 };
