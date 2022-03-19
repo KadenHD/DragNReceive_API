b628b4b4b50c4b8af954a861361d67217105ae5c
KadenHD <pierraored@gmail.com>
2022-03-19 17:37:16 +0100

    Update index.spec.js

--- test/puppeteer/index.spec.js
+++ test/puppeteer/index.spec.js
@@ -13,15 +13,22 @@ const delay = 1000;
 
 try {
     console.log(`${ChalkMSG.INFO}Launching the Puppeteer test...`);
+
+    console.log(`${ChalkMSG.INFO}Launching the test for Unlog...`);
     await unlogSpec(browser, url, delay);
-    // await partnerSpec(browser, url, delay);
-    // await sadminSpec(browser, url, delay);
+    console.log(`${ChalkMSG.SUCCESS}The test for Unlog is done.`);
+
+    console.log(`${ChalkMSG.INFO}Launching the test for Partner...`);
+    await partnerSpec(browser, url, delay);
+    console.log(`${ChalkMSG.SUCCESS}The test for Partner is done.`);
+
+    console.log(`${ChalkMSG.INFO}Launching the test for SuperAdmin / Admin...`);
+    await sadminSpec(browser, url, delay);
+    console.log(`${ChalkMSG.SUCCESS}The test for SuperAdmin / Admin is done.`);
+
     await browser.close();
     console.log(`${ChalkMSG.SUCCESS}The Puppeteer test is successfully done.`);
 } catch {
     console.log(`${ChalkMSG.ERROR}An error occured during the Puppeteer test.`);
     await browser.close();
 }
-
-
-
