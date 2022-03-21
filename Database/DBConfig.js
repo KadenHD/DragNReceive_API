9d3fe516352e54249a403650d8a70d5a3237227a
KadenHD <pierraored@gmail.com>
2022-03-21 19:22:44 +0100

    Update DBConfig.js

--- Database/DBConfig.js
+++ Database/DBConfig.js
@@ -1,8 +1,7 @@
 import Sequelize from "sequelize";
 import dotenv from 'dotenv';
-
 dotenv.config();
-/* Initialize Sequelize with parameters */
+
 const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
