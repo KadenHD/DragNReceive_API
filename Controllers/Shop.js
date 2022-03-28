63e16dfbe6f06a53ad3b3e4b5078ae3bff29bb57
KadenHD <pierraored@gmail.com>
2022-03-28 17:36:00 +0200

    Update Shop.js

--- Controllers/Shop.js
+++ Controllers/Shop.js
@@ -4,12 +4,11 @@ import { mkShop, writeShop, rmUser } from '../Scripts/FileSystems.js';
 import { createdShop, deletedShop, deletedUser } from '../Scripts/NodeMailer.js';
 
 export const findAllShops = async (req, res) => {
-    let data = await Shop.findAll();
-    scopedShops(req.currentUser, data)
-        .then(data => {
-            res.status(200).json(data);
+    scopedShops(req.currentUser, await Shop.findAll())
+        .then(result => {
+            res.status(200).json(result);
         })
-        .catch(err => {
+        .catch(() => {
             res.status(500).json({
                 error: `Une erreur est survenue de lors de la récupération des boutiques.`
             });
@@ -18,14 +17,14 @@ export const findAllShops = async (req, res) => {
 
 export const createShop = (req, res) => {
     Shop.create(req.body)
-        .then(data => {
+        .then(() => {
             mkShop(req.body.id);
             createdShop(req.body);
             res.status(200).json({
                 success: `La boutique a bien été créée.`
             });
         })
-        .catch(err => {
+        .catch(() => {
             res.status(500).json({
                 error: `Une erreur est survenue lors de la création de la boutique.`
             });
@@ -34,7 +33,7 @@ export const createShop = (req, res) => {
 
 export const findOneShop = (req, res) => {
     try {
-        res.status(200).json(req.shop)
+        res.status(200).json(req.shop);
     } catch (err) {
         res.status(500).json({
             error: `Une erreur est survenue lors de la recherche de la boutique.`
@@ -44,7 +43,7 @@ export const findOneShop = (req, res) => {
 
 export const deleteShop = (req, res) => {
     Shop.update(req.body, { where: { id: req.params.id } })
-        .then(async num => {
+        .then(async () => {
             deletedShop(req.shop)
             for (let i = 0; i < req.shop.dataValues.products.length; i++) {
                 if (req.shop.dataValues.products[i].deleted === false) {
@@ -60,7 +59,7 @@ export const deleteShop = (req, res) => {
                 success: `La boutique a bien été désactivée.`
             });
         })
-        .catch(err => {
+        .catch(() => {
             res.status(500).json({
                 error: `Une erreur est survenue de lors de la suppression de la boutique.`
             });
@@ -69,7 +68,7 @@ export const deleteShop = (req, res) => {
 
 export const updateShop = (req, res) => {
     Shop.update(req.body, { where: { id: req.params.id } })
-        .then(num => {
+        .then(() => {
             if (req.files) { /* save img */
                 writeShop(req.params.id, req.files.logo);
             }
@@ -77,7 +76,7 @@ export const updateShop = (req, res) => {
                 success: `La boutique a bien été modifiée`
             });
         })
-        .catch(err => {
+        .catch(() => {
             res.status(500).json({
                 error: `Une erreur est survenue de lors de la modification de la boutique.`
             });
