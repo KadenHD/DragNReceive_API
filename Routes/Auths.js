1c25f39ecd2faa08933efbe3365dd7ab47be5023
KadenHD <71968563+KadenHD@users.noreply.github.com>
2022-04-04 12:12:19 +0200

    Update Auths.js

--- Routes/Auths.js
+++ Routes/Auths.js
@@ -1,9 +1,9 @@
 import express from 'express';
 
-import { loginUser, loginClient, getCurrentUser, createForgotUser, updateResetUser } from '../Controllers/Auth.js';
+import { loginUser, loginClient, getCurrentUser, createForgotUser, updateResetUser, createForgotClient, updateResetClient } from '../Controllers/Auth.js';
 import { createUser } from '../Controllers/User.js';
 import { validFormCreateUser } from '../Permissions/Users.js';
-import { authRegisterClient, validForgotUser, validResetUser } from '../Permissions/Auths.js';
+import { authRegisterClient, validForgotUser, validResetUser, validForgotClient, validResetClient } from '../Permissions/Auths.js';
 
 const router = express.Router();
 
@@ -13,5 +13,7 @@ router.post('/register', authRegisterClient, validFormCreateUser, createUser);
 router.get('/currentUser', getCurrentUser);
 router.post('/forgotUser', validForgotUser, createForgotUser);
 router.post('/resetUser/:userId/:token', validResetUser, updateResetUser);
+router.post('/forgotClient', validForgotClient, createForgotClient);
+router.post('/resetClient/:userId/:id', validResetClient, updateResetClient);
 
 export default router;
