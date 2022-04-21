d528853f529906e9b561363c75d77406f0d5ffba
KadenHD <pierraored@gmail.com>
2022-04-21 22:05:41 +0200

    Update Orders.js

--- Routes/Orders.js
+++ Routes/Orders.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
-import { findAllOrder, createOrder, findOneOrder, updateOrder } from '../Controllers/Order.js';
-import { setOrder, authCreateOrder, validFormCreateOrder, validFormUpdateOrder } from '../Permissions/Orders.js';
+import { findAllOrder, createOrder, findOneOrder, updateOrder, cancelOrder } from '../Controllers/Order.js';
+import { setOrder, authCreateOrder, validFormCreateOrder, validFormUpdateOrder, validFormCancelOrder } from '../Permissions/Orders.js';
 
 const router = express.Router();
 
@@ -9,5 +9,6 @@ router.get('/', findAllOrder);
 router.post('/', authCreateOrder, validFormCreateOrder, createOrder);
 router.get('/:id', setOrder, findOneOrder)
 router.put('/:id', setOrder, validFormUpdateOrder, updateOrder);
+router.post('/cancelOrder', validFormCancelOrder, cancelOrder);
 
 export default router;
